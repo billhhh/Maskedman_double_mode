4cc54c8ec25d0262a92a27c342a8aca4f86c5595
Bill Dufresne <371570273@qq.com>
2017-05-23 15:40:44 +0800

    ASEF 李斌发cpp

---
+++ ASEF 李斌发cpp/asef.h
@@ -0,0 +1,23 @@
+#ifndef _ASEF_H_
+#define _ASEF_H_
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+typedef struct{
+	CvMat *lfilter, *rfilter;
+	int n_rows, n_cols;
+	CvRect lrect, rrect;
+	CvMat *lfilter_dft, *rfilter_dft;
+	CvMat *image, *image_tile;
+	CvMat *lcorr, *rcorr;
+	CvMat *lroi, *rroi;
+	CvMat *lut;
+	CvMat face_img;
+} AsefEyeLocator;
+
+void asef_initialze(AsefEyeLocator *asef, const char *file_name);
+void asef_destroy(AsefEyeLocator *asef);
+void asef_locate_eyes(AsefEyeLocator *asef, IplImage *image, CvRect face_rect, CvPoint *leye, CvPoint *reye);
+
+#endif
